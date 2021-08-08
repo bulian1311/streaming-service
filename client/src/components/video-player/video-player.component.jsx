d4aa1a9001360fa511ec1aa9f30840823ef5a41d
bulian1311 <bulian1311@gmail.com>
2021-08-08 12:00:48 +0300

    hls player

--- client/src/components/video-player/video-player.component.jsx
+++ client/src/components/video-player/video-player.component.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
+// import videojs from 'video.js';
+// import 'video.js/dist/video-js.css';
 
-export const VideoPlayer = ({ options }) => {
-  const videoRef = React.useRef(null);
+export const VideoPlayer = () => {
+  const videoRef = React.useRef();
 
-  const VideoHtml = (props) => (
+  // React.useEffect(() => {
+  //   const videoElement = videoRef.current;
+  //   let player;
+  //   if (videoElement) {
+  //     player = videojs(videoElement, options, () => {
+  //       console.log('player is ready');
+  //       videoElement.muted = true;
+  //       videoElement.volume = 1;
+  //     });
+  //   }
+  //   return () => {
+  //     if (player) {
+  //       player.dispose();
+  //     }
+  //   };
+  // }, [options]);
+
+  return (
     <div data-vjs-player>
       <video ref={videoRef} className="video-js vjs-big-play-centered" />
     </div>
   );
-
-  React.useEffect(() => {
-    const videoElement = videoRef.current;
-    let player;
-    if (videoElement) {
-      player = videojs(videoElement, options, () => {
-        console.log('player is ready');
-        videoElement.muted = true;
-        videoElement.volume = 1;
-      });
-    }
-    return () => {
-      if (player) {
-        player.dispose();
-      }
-    };
-  }, [options]);
-
-  return <VideoHtml />;
 };
 export default VideoPlayer;
--- client/src/pages/home/home.page.jsx
+++ client/src/pages/home/home.page.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { VideoPlayer } from '../../components';
+import { HlsPlayer } from '../../components';
 
 export const HomePage = () => {
-  const videoJsOptions = {
-    autoplay: true,
-    controls: true,
-    responsive: true,
-    fluid: true,
-    sources: [
-      {
-        src: '/videos/output.m3u8',
-        type: 'application/x-mpegURL',
-      },
-    ],
-  };
-
   return (
     <div>
-      <p>homepage</p>
-      <VideoPlayer options={videoJsOptions} />
+      <p>Homepage</p>
+      <HlsPlayer />
       <p>homepage</p>
       <p>homepage</p>
       <p>homepage</p>
