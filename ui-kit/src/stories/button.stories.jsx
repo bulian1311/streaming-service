86225f7ec60b5c6346f44c72e3129bebb78a335d
bulian1311 <bulian1311@gmail.com>
2021-09-29 18:09:19 +0300

    ui-kit

--- ui-kit/src/stories/button.stories.jsx
+++ ui-kit/src/stories/button.stories.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+ import React from 'react';
+// import { storiesOf } from '@storybook/react';
 
-import { Button } from '../components/button';
+ import { Button } from '../components/button';
 
-const stories = storiesOf('Button', module);
+// const stories = storiesOf('Button', module);
 
-stories.add('Button regular', () => {
-  return <Button>Test button</Button>;
-});
+// stories.add('Button regular', () => {
+//   return <Button>Test button</Button>;
+// });
 
-stories.add('Button reversed', () => {
-  return <Button reverse>Test button</Button>;
-});
+// stories.add('Button reversed', () => {
+//   return <Button reverse>Test button</Button>;
+// });
 
-stories.add('Button loading', () => {
-  return <Button isLoading>Test button</Button>;
-});
+// stories.add('Button loading', () => {
+//   return <Button isLoading>Test button</Button>;
+// });
 
-stories.add('Button reversed loading', () => {
-  return (
-    <Button reverse isLoading>
-      Test button
-    </Button>
-  );
-});
+// stories.add('Button reversed loading', () => {
+//   return (
+//     <Button reverse isLoading>
+//       Test button
+//     </Button>
+//   );
+// });
+
+export default {
+  title: "Button",
+  component: Button
+};
+
+export const DefaultButton = () => <Button>Button</Button>
+
